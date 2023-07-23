72f7c8ab8d069d8b1201538f257ab0a0c4cbee76
CaroHolt <caro.holt.0208@gmail.com>
2023-07-23 17:53:26 +0200

    Update layer.py

--- src/transformers/adapters/layer.py
+++ src/transformers/adapters/layer.py
@@ -46,7 +46,10 @@ class AdapterLayerBase(ABC):
 
     def _store_gating_score(self, adapter_name, gating_score):
         context = ForwardContext.get_context()
+        print("context")
+        print('************************')
         print(context)
+        print('************************')
         if context.output_adapter_gating_scores:
             gating_cache = context.adapter_gating_scores
             if self.layer_idx not in gating_cache[adapter_name]:
