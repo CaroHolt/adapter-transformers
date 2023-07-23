c630523d350c4b3f7fa0c98271f4f1bba80c83d5
CaroHolt <caro.holt.0208@gmail.com>
2023-07-23 18:29:05 +0200

    Update layer.py

--- src/transformers/adapters/layer.py
+++ src/transformers/adapters/layer.py
@@ -233,6 +233,10 @@ class AdapterLayer(AdapterLayerBase, nn.Module):
                 adapter_layer = self.adapters[adapter_stack_layer]
                 hidden_states, _, residual = adapter_layer.pre_forward(hidden_states, input_tensor, layer_norm)
                 context = ForwardContext.get_context()
+                print("context")
+                print('************************')
+                print(context)
+                print('************************')
                 layer_output = adapter_layer(
                     hidden_states, residual_input=residual, output_gating=context.output_adapter_gating_scores
                 )
